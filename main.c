6ad2233f1fc7456f7280d2fc6ae9ebff7dbe86fc
Uri Geva <urigeva93@gmail.com>
2017-09-19 15:11:42 +0300

    sdfds

--- main.c
+++ main.c
@@ -11,8 +11,8 @@ int main(int argc, char* argv[]) {
 
 	if(argc == 2 && strcmp(argv[1], "-c") == 0)  //CONSOLE MODE
 		return activeconsole();
-	//else if(argc == 2 && strcmp(argv[1], "-g") == 0) //GUI MODE
-	//	return activegui();
+	else if(argc == 2 && strcmp(argv[1], "-g") == 0) //GUI MODE
+		return activegui();
 	else if(argc == 1) //DEFAULT MODE - CONSOLE
 		return activeconsole();
 
