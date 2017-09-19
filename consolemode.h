68f2ff20b404275e85ff2feb0fa06568e88351ef
Uri Geva <urigeva93@gmail.com>
2017-09-19 15:09:03 +0300

    make America great again

---
+++ consolemode.h
@@ -0,0 +1,15 @@
+/*
+ * consolemode.h
+ *
+ *  Created on: 19 בספט׳ 2017
+ *      Author: uri
+ */
+
+#ifndef CONSOLEMODE_H_
+#define CONSOLEMODE_H_
+
+#include "SPCHESSMainAux.h"
+
+int activeconsole();
+
+#endif /* CONSOLEMODE_H_ */
